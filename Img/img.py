6facc3857041690dfb7973b85cfd493af8416860
sev2023 <114661729+sev2023@users.noreply.github.com>
2023-03-16 01:50:12 +0000

    image processing color selection

--- Img/img.py
+++ Img/img.py
@@ -29,7 +29,20 @@ plt.show()
 plt.imshow(pic[:,::2])
 plt.show()
 
-#
+# show part
 plt.imshow(pic[300:450, 150:600])
 plt.show()
+
+# colors change
+plt.imshow(pic + 100)
+plt.show()
+
+# colors selection - show high Red
+pic = np.array(io.imread('fire.jpeg'))
+# -- destructive process require reloading
+mask = pic[:,:,0] < 200  # all colors with Low Red
+pic[mask] == 255 # Low Red reset to 255 (white)
+plt.imshow(pic)
+plt.show()
+
 
