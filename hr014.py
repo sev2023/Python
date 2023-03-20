4826a51fc411e68f68fdcf6d3e8c801746b988be
sev2023 <114661729+sev2023@users.noreply.github.com>
2023-03-20 13:08:07 +0000

    matrix sub / dot / cross

--- hr014.py
+++ hr014.py
@@ -12,10 +12,10 @@ class Points(object):
         return Points(self.x - no.x, self.y - no.y, self.z - no.z)
     
     def dot(self, no):
-        pass     ######### NO IDEA
+        return self.x * no.x + self.y * no.y + self.z * no.z
     
     def cross(self, no):
-        pass    ########## NO IDEA
+        return Points(self.y * no.z - self.z * no.y, self.z * no.x - self.x * no.z, self.x * no.y - self.y * no.x)
     
     def absolute(self):
         return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
