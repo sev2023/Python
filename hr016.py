9ce8660c4b5948fc465b1e2e51ae6656710d2d80
sev2023 <114661729+sev2023@users.noreply.github.com>
2023-03-25 02:38:57 +0000

    cool getattr() again

---
+++ hr016.py
@@ -0,0 +1,20 @@
+# INPUT:
+#
+# 16
+# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 24 52
+# 4
+# intersection_update 10
+# 2 3 5 6 8 9 1 4 7 11
+# update 2
+# 55 66
+# symmetric_difference_update 5
+# 22 7 35 62 58
+# difference_update 7
+# 11 22 35 55 58 62 66
+#
+
+input()
+sett = set(input().split())
+for _ in range(int(input())):
+    getattr(sett, input().split()[0])(set(input().split()))
+print(sum(map(int, sett)))
