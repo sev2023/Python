e7af412734349b64859da4ad7a9d695b2739327e
sev2023 <114661729+sev2023@users.noreply.github.com>
2023-07-10 15:13:44 +0100

    Update longest-substring-without-repeating-characters.py

--- leet/longest-substring-without-repeating-characters.py
+++ leet/longest-substring-without-repeating-characters.py
@@ -4,13 +4,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         res = 0
-        for i in range(len(s)):
-            for j in range(i, len(s)+1):
-                print(s[i:j])
+
+        i = 0
+        while i < len(s) - res:
+            
+            rr = 0              
+            for j in range(i+1, len(s)+1):
                 if len(s[i:j]) != len(set(s[i:j])): break
-                if len(s[i:j]) > res:
-                    res = len(s[i:j])
-                
+                rr += 1
+            if rr  > res:
+                res = rr
+            i += 1
+            
         # print([s[i:j] for i in range(len(s)) for j in range(i, len(s))])
         return  res
       
